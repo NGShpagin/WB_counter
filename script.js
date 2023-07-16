be9e2b835f80358ee24675792c1d1ab2c2c69049
NGShpagin <shpagin9393@mail.ru>
2023-07-16 14:46:20 +0300

    some fixes

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const primeCost = document.querySelector('#prime-cost')
 const finalPriceEl = document.querySelector('#sold');
-const dimsFormEl = document.querySelector('.form__dims');
-const sizeEl = dimsFormEl.querySelector('.size');
+const dimsFormEl = document.querySelector('.dims');
+const sizeEl = document.querySelector('.dims__size');
 const btnEl = document.querySelector('.button');
-const categoriesEl = document.querySelector('.form__categories');
+const categoriesEl = document.querySelector('.categories');
 const categoryEl = categoriesEl.querySelector('#category');
 const chargeEl = categoriesEl.querySelector('.charge');
 const lastMileEl = document.querySelector('.last-mile-final');
 const IEEl = document.querySelector('.IE-final');
 const acquiringEl = document.querySelector('.acquiring-final');
-const inputEls = document.querySelectorAll('input');
 const errorTxtEl = document.querySelector('.error-text');
 const incomeFinalEl = document.querySelector('.income-final');
 const minPrice = document.querySelector('.min-cost')
 const minAvailablePrice = document.querySelector('.min-available-cost')
+const necessaryEls = document.querySelectorAll('.necessary');
 
-console.log(btnEl)
+necessaryEls.forEach((el) => {
+    el.querySelector('h3').innerHTML += '<span style="color: red"> *</span>';
+})
+
 
 const sizeFunc = function () {
     const height = dimsFormEl.querySelector('#height');
@@ -26,15 +29,25 @@ const sizeFunc = function () {
 
 btnEl.addEventListener('click', (e) => {
     let flag = false;
-    inputEls.forEach((element) => {
-        if (element.value === '') {
-            element.classList.add('error')
+    necessaryEls.forEach((element) => {
+        console.log(element)
+        if (element.querySelector('input') && element.querySelector('input').value === '') {
+            element.querySelector('input').classList.add('error');
             flag = true;
-            console.log(`${element.value} - ${flag}`)
+        } else if (element.querySelector('select') &&
+            element.querySelector('select').value === '') {
+            element.querySelector('select').classList.add('error');
+            console.log(element.querySelector('select').value)
+            flag = true;
         } else {
-            if (element.classList.contains('error')) {
-                element.classList.remove('error')
-                element.style.border = '1px solid black'
+            if (element.querySelector('input') &&
+                element.querySelector('input').classList.contains('error')) {
+                element.querySelector('input').classList.remove('error');
+                element.querySelector('input').style.border = '1px solid black';
+            } else if (element.querySelector('select') &&
+                element.querySelector('select').classList.contains('error')) {
+                element.querySelector('select').classList.remove('error');
+                element.querySelector('select').style.border = '1px solid black';
             }
         }
     })
@@ -43,6 +56,7 @@ btnEl.addEventListener('click', (e) => {
         const errorEl = document.querySelectorAll('.error');
         errorEl.forEach((item) => {
             item.style.border = '1px solid red'
+            console.log(item)
         })
         e.preventDefault();
     } else {
@@ -87,6 +101,6 @@ finalPriceEl.addEventListener('input', () => {
 
     IEEl.textContent = (Number(finalPriceEl.value) * 0.06).toFixed(2) + ' руб';
 
-    acquiringEl.textContent = (Number(finalPriceEl.value) * 0.015).toFixed(2)  + ' руб';
+    acquiringEl.textContent = (Number(finalPriceEl.value) * 0.015).toFixed(2) + ' руб';
 })
 
